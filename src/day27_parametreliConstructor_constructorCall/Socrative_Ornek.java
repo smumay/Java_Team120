7500921c0c3b068f0ba498cf424043d92da9b597
Your Name <you@example.com>
2023-07-31 09:49:16 +0200

    20230731.v2

---
+++ src/day27_parametreliConstructor_constructorCall/Socrative_Ornek.java
@@ -0,0 +1,12 @@
+package day27_parametreliConstructor_constructorCall;
+
+public class Socrative_Ornek {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello GitHub");
+
+
+    }
+}
